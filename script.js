4f2c55808f67cfddbe2ec26fde6a140bf2998239
ortizfr724 <ortizjr.franklin@gmail.com>
2025-01-03 19:51:44 -0400

    Initial commit
    Add index script files
    
    Add notes to script file first
    sub-problem

---
+++ script.js
@@ -0,0 +1,9 @@
+// console.log('Hello World!');
+
+
+// Create a function getComputerChoice()
+// Within the function return a random number using Math.random()
+// Create if, else if, esle statements for the string values 'rock', 'paper', 'scissors'
+// Test function using the return statement and console.log()
+
+
